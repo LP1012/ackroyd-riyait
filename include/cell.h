4eaef759997a6d418349f4637c54164679693c01
Liam Pohlmann <lpgeorge2@gmail.com>
2026-03-23 16:12:59 -0500

    add const reference to Material

--- include/cell.h
+++ include/cell.h
@@ -1,17 +1,20 @@
 #ifndef ACKROYD_RIYAIT_CELL_H_
 #define ACKROYD_RIYAIT_CELL_H_
 
+#include "material.h"
 #include "point.h"
 
 namespace ar {
 class Cell {
  public:
-  Cell(const Point cell_center, const double dx, const double dy);
+  Cell(const Point cell_center, const double dx, const double dy,
+       const Material& material);
 
  private:
   const Point cell_center_;
   const double dx_;
   const double dy_;
+  const Material& material_;
 };
 }  // namespace ar
 
--- src/cell.cc
+++ src/cell.cc
@@ -1,6 +1,7 @@
 #include "cell.h"
 
 namespace ar {
-Cell::Cell(const Point cell_center, const double dx, const double dy)
-    : cell_center_(cell_center), dx_(dx), dy_(dy) {}
+Cell::Cell(const Point cell_center, const double dx, const double dy,
+           const Material& material)
+    : cell_center_(cell_center), dx_(dx), dy_(dy), material_(material) {}
 }  // namespace ar
