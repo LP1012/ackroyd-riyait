624bf5a03a7b1a05ac5bcd14add093c2d7623a06
Liam Pohlmann <liam.pohlmann@gmail.com>
2026-03-23 19:16:49 -0500

    create method to set the cell source -- needs to be checked!!!

--- src/cell.cc
+++ src/cell.cc
@@ -10,11 +10,18 @@ Cell::Cell(const double xmin, const double xmax, const double ymin,
       material_(material),
       dx_(xmax - xmin),
       dy_(ymax - ymin),
-      cell_center_(ComputeCellCenter()) {}
+      cell_center_(ComputeCellCenter()) {
+  cell_source_ = material_.IsotropicSource();  // initialize source value
+}
 
 Point Cell::ComputeCellCenter() {
   double x_coord = xmin_ + dx_ / 2.0;
   double y_coord = ymin_ + dy_ / 2.0;
   return Point(x_coord, y_coord);
+}
+
+void Cell::SetCellSource(const double cell_scalar_flux) {
+  cell_source_ = material_.scattering_xs() * cell_scalar_flux +
+                 material_.IsotropicSource();  // check this!!!
 }
 }  // namespace ar
