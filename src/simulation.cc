b6f3712acdfba8df9bd8e2aaf18c9807707ee987
Liam Pohlmann <liam.pohlmann@gmail.com>
2026-03-23 19:55:46 -0500

    add southeast sweep

--- src/simulation.cc
+++ src/simulation.cc
@@ -63,6 +63,27 @@ std::vector<std::vector<double>> Simulation::SweepNorthWest(
   return scalar_flux_contribution;
 }
 
+std::vector<std::vector<double>> Simulation::SweepSouthEast(
+    const double quadrature_weight, const double x_cosine,
+    const double y_cosine) {
+  int n_cols = cells_[0].size();
+  int n_rows = cells_.size();
+
+  std::vector<std::vector<double>> scalar_flux_contribution(
+      n_rows, std::vector<double>(n_cols, 0.0));  // preallocates to all zeros
+
+  for (auto j = n_rows - 1; j > -1; j--) {
+    for (auto i = 0; i > n_cols; i++) {
+      double cell_center_flux = SweepStep(x_cosine, y_cosine, cells_[i][j]);
+      scalar_flux_contribution[i][j] += cell_center_flux * quadrature_weight;
+      cells_[i][j].SetCenterFlux(cell_center_flux);
+      cells_[i][j].SetEastFlux();
+      cells_[i][j].SetSouthFlux();
+    }
+  }
+  return scalar_flux_contribution;
+}
+
 double Simulation::SweepStep(const double x_cosine, const double y_cosine,
                              Cell &cell) {
   double east_west_flux = cell.west_flux();
