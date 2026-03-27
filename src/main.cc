33144a91749636445d5ac3e9b4ae229b276a671d
Liam Pohlmann <liam.pohlmann@gmail.com>
2026-03-27 17:42:22 -0500

    create materials and regions

--- src/main.cc
+++ src/main.cc
@@ -1,7 +1,36 @@
 #include <iostream>
 
+#include "material.h"
+#include "rectangular_region.h"
+
+using namespace ar;
+
 int main() {
-  std::cout << "Hello World" << std::endl;
+  // hard-code in the regions and materials for now
+  Material material_1(0.19, 0.2, 6.4);
+  Material material_2(0, 0);
+  Material material_3(0.19, 0.2);
+
+  // Code is blocked from top row to bottom row. Vacuum boundary conditions are
+  // assumed for all sides.
+
+  RectangularRegion region_1(material_3, -10, 10, 5, 10, 2, 2);
+
+  RectangularRegion region_2(material_3, -10, -5, 1.25, 5, 2, 2);
+  RectangularRegion region_3(material_2, -5, 5, 1.25, 5, 2, 2);
+  RectangularRegion region_4(material_3, 5, 10, 1.25, 5, 2, 2);
+
+  RectangularRegion region_5(material_3, -10, -5, -1.25, 1.25, 2, 2);
+  RectangularRegion region_6(material_2, -5, -1.25, -1.25, 1.25, 2, 2);
+  RectangularRegion region_7(material_1, -1.25, 1.25, -1.25, 1.25, 2, 2);
+  RectangularRegion region_8(material_2, 1.25, 5, -1.25, 1.25, 2, 2);
+  RectangularRegion region_9(material_3, 5, 10, -1.25, 1.25, 2, 2);
+
+  RectangularRegion region_10(material_3, -10, 5, -5, -1.25, 2, 2);
+  RectangularRegion region_11(material_2, -5, 5, -5, -1.25, 2, 2);
+  RectangularRegion region_12(material_3, 5, 10, -5, -1.25, 2, 2);
+
+  RectangularRegion region_13(material_3, -10, 10, -10, -5, 2, 2);
 
   return 0;
 }
