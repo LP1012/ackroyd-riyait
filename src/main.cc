b42ecb81a5fb69db24293235b1a9d5c3900de0fa
Liam Pohlmann <liam.pohlmann@gmail.com>
2026-03-27 17:44:58 -0500

    add simulation object

--- src/main.cc
+++ src/main.cc
@@ -2,6 +2,7 @@
 
 #include "material.h"
 #include "rectangular_region.h"
+#include "simulation.h"
 
 using namespace ar;
 
@@ -35,6 +36,8 @@ int main() {
   std::vector<RectangularRegion> regions = {
       region_1, region_2, region_3,  region_4,  region_5,  region_6, region_7,
       region_8, region_9, region_10, region_11, region_12, region_13};
+
+  Simulation ar_simulation(regions);
 
   return 0;
 }
