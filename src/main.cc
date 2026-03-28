67edec663675aaa83dbac45147cc4d49524a379b
Liam Pohlmann <liam.pohlmann@gmail.com>
2026-03-28 03:11:54 -0500

    update domain to take square cell width

--- src/main.cc
+++ src/main.cc
@@ -20,23 +20,31 @@ int main() {
   // Code is blocked from top row to bottom row. Vacuum boundary conditions are
   // assumed for all sides.
 
-  RectangularRegion region_1(material_3, -10, 10, 5, 10, 3, 3);
+  double target_cell_width =
+      1.25;  // take multiples of this to get a grid convergence study
 
-  RectangularRegion region_2(material_3, -10, -5, 1.25, 5, 3, 3);
-  RectangularRegion region_3(material_2, -5, 5, 1.25, 5, 3, 3);
-  RectangularRegion region_4(material_3, 5, 10, 1.25, 5, 3, 3);
+  RectangularRegion region_1(material_3, -10, 10, 5, 10, target_cell_width);
 
-  RectangularRegion region_5(material_3, -10, -5, -1.25, 1.25, 3, 3);
-  RectangularRegion region_6(material_2, -5, -1.25, -1.25, 1.25, 3, 3);
-  RectangularRegion region_7(material_1, -1.25, 1.25, -1.25, 1.25, 3, 3);
-  RectangularRegion region_8(material_2, 1.25, 5, -1.25, 1.25, 3, 3);
-  RectangularRegion region_9(material_3, 5, 10, -1.25, 1.25, 3, 3);
+  RectangularRegion region_2(material_3, -10, -5, 1.25, 5, target_cell_width);
+  RectangularRegion region_3(material_2, -5, 5, 1.25, 5, target_cell_width);
+  RectangularRegion region_4(material_3, 5, 10, 1.25, 5, target_cell_width);
 
-  RectangularRegion region_10(material_3, -10, -5, -5, -1.25, 3, 3);
-  RectangularRegion region_11(material_2, -5, 5, -5, -1.25, 3, 3);
-  RectangularRegion region_12(material_3, 5, 10, -5, -1.25, 3, 3);
+  RectangularRegion region_5(material_3, -10, -5, -1.25, 1.25,
+                             target_cell_width);
+  RectangularRegion region_6(material_2, -5, -1.25, -1.25, 1.25,
+                             target_cell_width);
+  RectangularRegion region_7(material_1, -1.25, 1.25, -1.25, 1.25,
+                             target_cell_width);
+  RectangularRegion region_8(material_2, 1.25, 5, -1.25, 1.25,
+                             target_cell_width);
+  RectangularRegion region_9(material_3, 5, 10, -1.25, 1.25, target_cell_width);
 
-  RectangularRegion region_13(material_3, -10, 10, -10, -5, 3, 3);
+  RectangularRegion region_10(material_3, -10, -5, -5, -1.25,
+                              target_cell_width);
+  RectangularRegion region_11(material_2, -5, 5, -5, -1.25, target_cell_width);
+  RectangularRegion region_12(material_3, 5, 10, -5, -1.25, target_cell_width);
+
+  RectangularRegion region_13(material_3, -10, 10, -10, -5, target_cell_width);
 
   std::vector<RectangularRegion> regions = {
       region_1, region_2, region_3,  region_4,  region_5,  region_6, region_7,
