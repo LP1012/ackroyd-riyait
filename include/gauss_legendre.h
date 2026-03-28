02ced03ae47f41780246e5f44aa3eb7082776184
Liam Pohlmann <liam.pohlmann@gmail.com>
2026-03-28 17:51:30 -0500

    add docstrings, change member and getter name

--- include/gauss_legendre.h
+++ include/gauss_legendre.h
@@ -4,16 +4,40 @@
 #include <vector>
 
 namespace ar {
+/**
+ * @brief Defines an object holding weights and abscissas for integrating on the
+ * standard interval ([-1,1]) using Gauss-Legendre quadrature
+ *
+ */
 class GaussLegendre {
  public:
+  /**
+   * @brief Construct a new Gauss Legendre object
+   *
+   * @param n_vals Number of values desired
+   */
   GaussLegendre(const int n_vals);
 
+  /**
+   * @brief Get the weights
+   *
+   * @return std::vector<double>
+   */
   std::vector<double> GetWeights() const { return weights_; }
-  std::vector<double> GetAbscissae() const { return abscissae_; }
+
+  /**
+   * @brief Get the abscissas
+   *
+   * @return std::vector<double>
+   */
+  std::vector<double> GetAbscissas() const { return abscissas_; }
 
  private:
+  /// @brief Quadrature weights
   std::vector<double> weights_;
-  std::vector<double> abscissae_;
+
+  /// @brief Quadrature abscissas
+  std::vector<double> abscissas_;
 };
 }  // namespace ar
 
