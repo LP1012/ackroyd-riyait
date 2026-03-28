59dc4d77a28a53a9c09a630e1b8164ae10e84c6d
Liam Pohlmann <liam.pohlmann@gmail.com>
2026-03-28 01:59:29 -0500

    fix typo in region 10, set all number of cells in each region to be 3x3

--- src/main.cc
+++ src/main.cc
@@ -20,23 +20,23 @@ int main() {
   // Code is blocked from top row to bottom row. Vacuum boundary conditions are
   // assumed for all sides.
 
-  RectangularRegion region_1(material_3, -10, 10, 5, 10, 2, 2);
+  RectangularRegion region_1(material_3, -10, 10, 5, 10, 3, 3);
 
-  RectangularRegion region_2(material_3, -10, -5, 1.25, 5, 2, 2);
-  RectangularRegion region_3(material_2, -5, 5, 1.25, 5, 2, 2);
-  RectangularRegion region_4(material_3, 5, 10, 1.25, 5, 2, 2);
+  RectangularRegion region_2(material_3, -10, -5, 1.25, 5, 3, 3);
+  RectangularRegion region_3(material_2, -5, 5, 1.25, 5, 3, 3);
+  RectangularRegion region_4(material_3, 5, 10, 1.25, 5, 3, 3);
 
-  RectangularRegion region_5(material_3, -10, -5, -1.25, 1.25, 2, 2);
-  RectangularRegion region_6(material_2, -5, -1.25, -1.25, 1.25, 2, 2);
-  RectangularRegion region_7(material_1, -1.25, 1.25, -1.25, 1.25, 2, 2);
-  RectangularRegion region_8(material_2, 1.25, 5, -1.25, 1.25, 2, 2);
-  RectangularRegion region_9(material_3, 5, 10, -1.25, 1.25, 2, 2);
+  RectangularRegion region_5(material_3, -10, -5, -1.25, 1.25, 3, 3);
+  RectangularRegion region_6(material_2, -5, -1.25, -1.25, 1.25, 3, 3);
+  RectangularRegion region_7(material_1, -1.25, 1.25, -1.25, 1.25, 3, 3);
+  RectangularRegion region_8(material_2, 1.25, 5, -1.25, 1.25, 3, 3);
+  RectangularRegion region_9(material_3, 5, 10, -1.25, 1.25, 3, 3);
 
-  RectangularRegion region_10(material_3, -10, 5, -5, -1.25, 2, 2);
-  RectangularRegion region_11(material_2, -5, 5, -5, -1.25, 2, 2);
-  RectangularRegion region_12(material_3, 5, 10, -5, -1.25, 2, 2);
+  RectangularRegion region_10(material_3, -10, -5, -5, -1.25, 3, 3);
+  RectangularRegion region_11(material_2, -5, 5, -5, -1.25, 3, 3);
+  RectangularRegion region_12(material_3, 5, 10, -5, -1.25, 3, 3);
 
-  RectangularRegion region_13(material_3, -10, 10, -10, -5, 2, 2);
+  RectangularRegion region_13(material_3, -10, 10, -10, -5, 3, 3);
 
   std::vector<RectangularRegion> regions = {
       region_1, region_2, region_3,  region_4,  region_5,  region_6, region_7,
