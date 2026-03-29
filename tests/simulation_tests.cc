bc4cdf5c0ac15bc0dc99fa7776528aadac7bd9fd
Liam Pohlmann <liam.pohlmann@gmail.com>
2026-03-29 18:42:00 -0500

    add spherical quadrature number

--- tests/simulation_tests.cc
+++ tests/simulation_tests.cc
@@ -11,5 +11,5 @@ TEST(SimulationTest, Constructor) {
   RectangularRegion region_2(material_2, 1, 2, 0, 1, 2, 2);
 
   std::vector<RectangularRegion> regions = {region_1, region_2};
-  EXPECT_NO_THROW(Simulation simulation(regions));
+  EXPECT_NO_THROW(Simulation simulation(regions, 4));
 }
