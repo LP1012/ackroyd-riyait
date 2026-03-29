b19c75a8e858224129ebc7e1072c8291d6057b97
Elijah Capps <ecapps2@illinois.edu>
2026-03-29 18:34:41 -0500

    rebuilding cells with fluxes, plotting

--- scripts/plot_flux.py
+++ scripts/plot_flux.py
@@ -2,9 +2,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 import numpy as np
-import os
-
-cwd = os.getcwd()
 
 def plot_cell_fluxes(csv_file):
     # Load CSV
@@ -18,19 +15,37 @@ def plot_cell_fluxes(csv_file):
     # Create plot
     fig, ax = plt.subplots()
 
-    # Plot flux
-    coords = np.array([])
-    flux_mesh = np.array([])
+    # Rebuild cell mesh with fluxes
+    xs = set()
+    ys = set()
+    flux_mesh = []
     curr_x = df.iloc[0]["x_center"]
 
-    temp_row = np.array([])
+    temp_row = []
     for _, row in df.iterrows():
         if row["x_center"] == curr_x:
-            np.append(temp_row)
+            temp_row.append(row["center_flux"])
+            xs.add(row["x_center"])
+            ys.add(row["y_center"])
+        else:
+            curr_x = row["x_center"]
+            flux_mesh.append(temp_row)
+            temp_row = []
 
-    X, Y = np.meshgrid(xs, ys)
+            temp_row.append(row["center_flux"])
+            xs.add(row["x_center"])
+            ys.add(row["y_center"])
 
-    ax.contourf(X, Y, flx)
+    flux_mesh.append(temp_row)
+    X, Y = np.meshgrid(np.array(list(xs)), np.array(list(ys)))
+
+    # Plot mesh
+    ax.pcolormesh(X, Y, flux_mesh, shading='gouraud')
+
+    # Labels
+    ax.set_xlabel("X")
+    ax.set_ylabel("Y")
+    ax.set_title("Scalar Flux over Problem Domain")
 
     plt.show()
 
