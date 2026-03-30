b52f7a0e28094b0b573384d265283a2b1c620d6b
Liam Pohlmann <liam.pohlmann@gmail.com>
2026-03-29 23:23:58 -0500

    test weight sum

--- tests/gauss_legendre_tests.cc
+++ tests/gauss_legendre_tests.cc
@@ -5,3 +5,16 @@
 using namespace ar;
 
 TEST(GaussLegendreTest, Constructor) { EXPECT_NO_THROW(GaussLegendre gl(2)); }
+
+TEST(GaussLegendreTest, WeightSum) {
+  for (auto i = 2; i <= 120; i += 2) {
+    GaussLegendre gl(i);
+
+    auto weights = gl.GetWeights();
+
+    double sum = 0;
+    for (auto weight : weights) sum += weight;
+
+    EXPECT_DOUBLE_EQ(sum, 2.0);
+  }
+}
