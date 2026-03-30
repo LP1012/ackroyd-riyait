52cbb1da62857a07ffecd056af870ee251b13b9a
Liam Pohlmann <liam.pohlmann@gmail.com>
2026-03-29 22:25:08 -0500

    fix setting issue with scattering cross section

--- src/material.cc
+++ src/material.cc
@@ -4,7 +4,7 @@ namespace ar {
 Material::Material(const int id, const double scattering_xs,
                    const double total_xs, const double source)
     : id_(id),
-      scattering_xs_(scattering_xs_),
+      scattering_xs_(scattering_xs),
       total_xs_(total_xs),
       source_(source) {}
 }  // namespace ar
