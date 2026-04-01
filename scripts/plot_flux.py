972dc9165cee9fd0095a0c8b8865b7321f036842
Liam Pohlmann <lpgeorge2@gmail.com>
2026-03-31 22:13:36 -0500

    modify heatmap to be smooth, remove labels

--- scripts/plot_flux.py
+++ scripts/plot_flux.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib.colors as mcolors
 
 
 def plot_cell_fluxes(csv_file):
@@ -25,7 +26,15 @@ def plot_cell_fluxes(csv_file):
 
     fig, ax = plt.subplots()
 
-    mesh = ax.pcolormesh(X, Y, flux_mesh, shading="nearest", cmap="magma")
+    mesh = ax.imshow(
+        flux_mesh,
+        norm=mcolors.LogNorm(),
+        cmap="magma",
+        interpolation="bicubic",
+        origin="lower",
+        extent=[xs.min(), xs.max(), ys.min(), ys.max()],
+        aspect="auto",
+    )
 
     # Add colorbar — this also confirms the value range at a glance
     cbar = fig.colorbar(mesh, ax=ax)
@@ -33,31 +42,35 @@ def plot_cell_fluxes(csv_file):
 
     ax.set_xlabel("X")
     ax.set_ylabel("Y")
-    ax.set_title("Scalar Flux over Problem Domain")
+    # ax.set_title("Scalar Flux over Problem Domain")
 
-    ax.set_xlim(-10, 10)
-    ax.set_ylim(-1, 10)
+    # ax.set_xlim(-10, 10)
+    # ax.set_ylim(-1, 10)
     # # ax.set_xticks(xs)
     # # ax.set_yticks(ys)
     # ax.grid(color="white", linewidth=0.5, linestyle="--", alpha=0.5)
 
+    ax.axis("off")
+    cbar.remove()
+
     plt.tight_layout()
-    plt.savefig("cell_fluxes.png", dpi=300)
+    plt.savefig("cell_fluxes.png", dpi=300, bbox_inches="tight", pad_inches=0)
 
     fig, ax = plt.subplots()
 
     diag_dist = np.diag(X)
     diag_flux = np.diag(flux_mesh)
-    mid = int(len(diag_dist)/2)
+    mid = int(len(diag_dist) / 2)
 
-    ax.plot(diag_dist[mid-1:], diag_flux[mid-1:])
+    ax.plot(diag_dist[mid - 1 :], diag_flux[mid - 1 :])
     ax.set_yscale("log")
 
     ax.set_xlabel("X-Y Diagonal")
     ax.set_ylabel("Scalar Flux")
-    
+
     plt.tight_layout()
     plt.savefig("diagonal_flux.png", dpi=300)
+
 
 if __name__ == "__main__":
     plot_cell_fluxes("cell_fluxes.csv")
