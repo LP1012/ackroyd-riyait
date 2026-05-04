bbfad62b459c6f8bac13183c50301b55b9d6765c
Liam Pohlmann <liam.pohlmann@gmail.com>
2026-05-04 00:33:10 -0500

    add a bunch of methods

--- python-fem/fem/even-parity-tris.py
+++ python-fem/fem/even-parity-tris.py
@@ -7,25 +7,29 @@ from scipy.integrate import dblquad
 from linear_triangles import *
 
 
-def mu(theta, phi):
+def Mu(theta, phi):
     return np.sin(phi) * np.cos(theta)
 
 
-def eta(theta, phi):
+def Eta(theta, phi):
     return np.sin(phi) * np.sin(theta)
 
 
-def K_mu_mu(vertices, j, n):
+def K_mu_mu(vertices, j, n, det_jacobian):
     result = dblquad(
-        K_mu_mu_integrand, 0, 1, lambda xi: 0, lambda xi: 1 - xi, args=(j, n, vertices)
+        K_mu_mu_integrand,
+        0,
+        1,
+        lambda xi: 0,
+        lambda xi: 1 - xi,
+        args=(j, n, vertices, det_jacobian),
     )
     return result
 
 
-def K_mu_mu_integrand(eta, xi, j, n, vertices):
+def K_mu_mu_integrand(eta, xi, j, n, vertices, det_jacobian):
     theta, phi = ReferenceToReal(vertices, xi, eta)
-    mu_sqrd = mu(theta, phi) ** 2
-    det_jacobian = DeterminantJacobian(vertices)
+    mu_sqrd = Mu(theta, phi) ** 2
     return (
         mu_sqrd
         * LinearBasis(xi, eta, j)
@@ -35,5 +39,162 @@ def K_mu_mu_integrand(eta, xi, j, n, vertices):
     )
 
 
-spatial_mesh = meshio.read("../gmsh/square-source-coarse.msh")
+def K_eta_eta(eta, xi, j, n, vertices, det_jacobian):
+    result = dblquad(
+        K_eta_eta_integrand,
+        0,
+        1,
+        lambda xi: 0,
+        lambda xi: 1 - xi,
+        args=(j, n, vertices, det_jacobian),
+    )
+    return result
+
+
+def K_eta_eta_integrand(eta, xi, j, n, vertices, det_jacobian):
+    theta, phi = ReferenceToReal(vertices, xi, eta)
+    eta_sqrd = Eta(theta, phi) ** 2
+    return (
+        eta_sqrd
+        * LinearBasis(xi, eta, j)
+        * LinearBasis(xi, eta, n)
+        * np.sin(phi)
+        * det_jacobian
+    )
+
+
+def K_mu_eta(eta, xi, j, n, vertices, det_jacobian):
+    result = dblquad(
+        K_mu_eta_integrand,
+        0,
+        1,
+        lambda xi: 0,
+        lambda xi: 1 - xi,
+        args=(j, n, vertices, det_jacobian),
+    )
+    return result
+
+
+def K_mu_eta_integrand(eta, xi, j, n, vertices, det_jacobian):
+    theta, phi = ReferenceToReal(vertices, xi, eta)
+    eta_mu = Eta(theta, phi) * Mu(theta, phi)
+    return (
+        eta_mu
+        * LinearBasis(xi, eta, j)
+        * LinearBasis(xi, eta, n)
+        * np.sin(phi)
+        * det_jacobian
+    )
+
+
+def M_xx(x_gradients, i, m, triangle_area):
+    return x_gradients[i] * x_gradients[m] * triangle_area
+
+
+def M_yy(y_gradients, i, m, triangle_area):
+    return y_gradients[i] * y_gradients[m] * triangle_area
+
+
+def M_xy(x_gradients, y_gradients, i, m, triangle_area):
+    return x_gradients[i] * y_gradients[m] * triangle_area
+
+
+def M_yx(x_gradients, y_gradients, i, m, triangle_area):
+    return x_gradients[i] * x_gradients[m] * triangle_area
+
+
+def B_mu(j, n, vertices, det_jacobian):
+    result = dblquad(
+        B_mu_integrand,
+        0,
+        1,
+        lambda xi: 0,
+        lambda xi: 1 - xi,
+        args=(j, n, vertices, det_jacobian),
+    )
+    return result
+
+
+def B_mu_integrand(eta, xi, j, n, vertices, det_jacobian):
+    theta, phi = ReferenceToReal(vertices, xi, eta)
+    mu = Mu(theta, phi)
+    return (
+        mu
+        * LinearBasis(xi, eta, j)
+        * LinearBasis(xi, eta, n)
+        * np.sin(phi)
+        * det_jacobian
+    )
+
+
+def B_eta(j, n, vertices, det_jacobian):
+    result = dblquad(
+        B_eta_integrand,
+        0,
+        1,
+        lambda xi: 0,
+        lambda xi: 1 - xi,
+        args=(j, n, vertices, det_jacobian),
+    )
+    return result
+
+
+def B_eta_integrand(eta, xi, j, n, vertices, det_jacobian):
+    theta, phi = ReferenceToReal(vertices, xi, eta)
+    eta = Eta(theta, phi)
+    return (
+        eta
+        * LinearBasis(xi, eta, j)
+        * LinearBasis(xi, eta, n)
+        * np.sin(phi)
+        * det_jacobian
+    )
+
+
+def K(j, n, vertices, det_jacobian):
+    result = dblquad(
+        K_integrand,
+        0,
+        1,
+        lambda xi: 0,
+        lambda xi: 1 - xi,
+        args=(j, n, vertices, det_jacobian),
+    )
+    return result
+
+
+def K_integrand(xi, eta, j, n, vertices, det_jacobian):
+    theta, phi = ReferenceToReal(vertices, xi, eta)
+    return (
+        LinearBasis(xi, eta, j) * LinearBasis(xi, eta, n) * np.sin(phi) * det_jacobian
+    )
+
+
+def M_matrix(verices):
+    area = TriangleArea(verices)
+    return (area / 6) * np.array([[2, 1, 1], [1, 2, 1], [1, 1, 2]], dtype=float)
+
+
+def K(n, vertices, det_jacobian):
+    result = dblquad(
+        A_integrand,
+        0,
+        1,
+        lambda xi: 0,
+        lambda xi: 1 - xi,
+        args=(n, vertices, det_jacobian),
+    )
+    return result
+
+
+def A_integrand(xi, eta, n, vertices, det_jacobian):
+    _, phi = ReferenceToReal(vertices, xi, eta)
+    return LinearBasis(xi, eta, n) * det_jacobian * np.sin(phi)
+
+
+spatial_mesh = meshio.read("../gmsh/square-source.msh")
 angle_mesh = meshio.read("../gmsh/solid-angle.msh")
+
+print(spatial_mesh)
+line_cells = next(cb.data for cb in spatial_mesh.cells if cb.type == "line")
+print(line_cells)
--- python-fem/fem/linear_triangles.py
+++ python-fem/fem/linear_triangles.py
@@ -13,6 +13,10 @@ def DeterminantJacobian(vertices):
     return np.abs(np.linalg.det(CreateJacobian(vertices)))
 
 
+def TriangleArea(vertices):
+    return DeterminantJacobian(vertices) / 2
+
+
 def ReferenceToReal(vertices, xi, eta):
     x1, y1 = vertices[0]
     x2, y2 = vertices[1]
@@ -34,3 +38,46 @@ def LinearBasis(xi, eta, i):
             return eta
         case _:
             raise ValueError(f"i must be 0, 1, or 2. i={i}")
+
+
+def LinearBasisXiDerivative(i):
+    match i:
+        case 0:
+            return -1
+        case 1:
+            return 1
+        case 2:
+            return 0
+        case _:
+            raise ValueError(f"i must be 0, 1, or 2. i={i}")
+
+
+def LinearBasisEtaDerivative(i):
+    match i:
+        case 0:
+            return -1
+        case 1:
+            return 0
+        case 2:
+            return 1
+        case _:
+            raise ValueError(f"i must be 0, 1, or 2. i={i}")
+
+
+def RealGradients(vertices, i):
+    jacobian = CreateJacobian(vertices)
+    xi_derivative = LinearBasisXiDerivative(i)
+    eta_derivative = LinearBasisEtaDerivative(i)
+    reference_gradients = np.array([xi_derivative, eta_derivative])
+    real_gradients = np.linalg.solve(jacobian, reference_gradients)
+    return real_gradients
+
+
+def BuildBasisDxDy(vertices):
+    dxs = np.zeros(3)
+    dys = np.zeros(3)
+    for i in range(3):
+        gradients = RealGradients(vertices, i)
+        dxs[i] = gradients[i]
+        dys[i] = gradients[i]
+    return dxs, dys
