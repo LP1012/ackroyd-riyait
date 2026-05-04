ac7428958f96cbd29917804e290694253ca8a627
Liam Pohlmann <liam.pohlmann@gmail.com>
2026-05-03 22:34:33 -0500

    add k mu mu

--- python-fem/fem/even-parity-tris.py
+++ python-fem/fem/even-parity-tris.py
@@ -1,6 +1,39 @@
 import meshio
+import numpy as np
+import scipy
+
+from scipy.integrate import dblquad
+
+from linear_triangles import *
+
+
+def mu(theta, phi):
+    return np.sin(phi) * np.cos(theta)
+
+
+def eta(theta, phi):
+    return np.sin(phi) * np.sin(theta)
+
+
+def K_mu_mu(vertices, j, n):
+    result = dblquad(
+        K_mu_mu_integrand, 0, 1, lambda xi: 0, lambda xi: 1 - xi, args=(j, n, vertices)
+    )
+    return result
+
+
+def K_mu_mu_integrand(eta, xi, j, n, vertices):
+    theta, phi = ReferenceToReal(vertices, xi, eta)
+    mu_sqrd = mu(theta, phi) ** 2
+    det_jacobian = DeterminantJacobian(vertices)
+    return (
+        mu_sqrd
+        * LinearBasis(xi, eta, j)
+        * LinearBasis(xi, eta, n)
+        * np.sin(phi)
+        * det_jacobian
+    )
+
 
 spatial_mesh = meshio.read("../gmsh/square-source-coarse.msh")
 angle_mesh = meshio.read("../gmsh/solid-angle.msh")
-
-print(spatial_mesh)
